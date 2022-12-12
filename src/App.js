b7e828e8562d2cf1d2c54d36f0d2ce8ecf22ee2a
passionatestar99939 <passionatestar.99939@outlook.com>
2022-12-12 13:40:24 -0800

    testing Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import Router from "./routes";
+import Router from "./routes"
 
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   return (
     <div>
+      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+        <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
+        <p className="text-gray-500 text-lg">
+          React and Tailwind CSS in action
+        </p>
+      </div>
       <Router />
     </div>
-  );
+  )
 }
 
-export default App;
-
+export default App
 
 // import { useState } from 'react';
 // import { InfoBox } from './components/InfoBox';
