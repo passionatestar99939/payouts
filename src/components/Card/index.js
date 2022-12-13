3ea6f01e2282cb62f8f7e237e5c96c4b464f5e44
passionatestar99939 <passionatestar.99939@outlook.com>
2022-12-13 08:58:58 -0800

    applied Tailwind CSS using sm:, md:, lg:

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,10 +6,10 @@ const Card = (props) => {
   const [isShowModal, setIsShowModal] = useState(false)
 
   return (
-    <div className="card" style={{ padding: "16px 20px" }}>
-      <div className="flex justify-between">
+    <div className="card sm:w-1/2 md:w-1/3: lg:w-1/4" style={{ padding: "16px 20px" }}>
+      <div className="flex">
         <img style={{ width: "16px", height: "16px" }} src="/card-img.png" />
-        <div className="text-xs">{props.title}</div>
+        <div className="text-xs"> {props.title}</div>
       </div>
       <div className="flex justify-between">
         <div>${props.value}</div>
